91620e2b502cf27966490c0cc345e3776872785c
miguel-sangar <miguelsgo2@gmail.com>
2025-08-11 09:30:44 +0200

    Ejercicio 2 subido

---
+++ ejercicio-01.js
@@ -0,0 +1,12 @@
+//Crea una variable llamada miSuperheroeFavorito y asigna el valor "Hulk":
+let miSuperheroeFavorito = "Hulk";
+//Crea una variable llamada numeroFavorito y asigna el valor 50:
+let numeroFavorito = 50;
+//Crea una variable llamada altura y asigna el valor 5:
+let altura = 5;
+//Crea una variable llamada longitud y asigna el valor 10:
+let longitud = 10;
+//Crea una variable llamada suma y asigna el valor de altura + longitud:
+let suma = altura + longitud;
+//Muestra todas las variables por consola:
+console.log(miSuperheroeFavorito, ", ", numeroFavorito, ", ", altura, ", ",longitud, ", ", suma);
---
+++ ejercicio-02.js
@@ -0,0 +1,5 @@
+const jedi = {nombre:"Luke Skywalker", edad: 19};
+
+jedi.edad = 25;
+
+console.log(jedi.edad);
